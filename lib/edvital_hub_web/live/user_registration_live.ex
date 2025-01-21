79a0e3f42ad0e0c7c145beebec2bec78b0332c2c
Rowa <jrowah@gmail.com>
2025-01-21 21:58:23 +0300

    render confirmation of registration

--- lib/edvital_hub_web/live/user_registration_live.ex
+++ lib/edvital_hub_web/live/user_registration_live.ex
@@ -3,47 +3,10 @@ defmodule EdvitalHubWeb.UserRegistrationLive do
 
   alias EdvitalHub.Accounts
   alias EdvitalHub.Accounts.User
+  alias EdvitalHubWeb.OnboardingLive.UserRegistration
+  alias EdvitalHubWeb.OnboardingLive.Shared.AuthComponents
 
-  def render(assigns) do
-    ~H"""
-    <div class="mx-auto max-w-sm">
-      <.header class="text-center">
-        Register for an account
-        <:subtitle>
-          Already registered?
-          <.link navigate={~p"/login"} class="font-semibold text-brand hover:underline">
-            Log in
-          </.link>
-          to your account now.
-        </:subtitle>
-      </.header>
-
-      <.simple_form
-        for={@form}
-        id="registration_form"
-        phx-submit="save"
-        phx-change="validate"
-        phx-trigger-action={@trigger_submit}
-        action={~p"/login?_action=registered"}
-        method="post"
-      >
-        <.error :if={@check_errors}>
-          Oops, something went wrong! Please check the errors below.
-        </.error>
-
-        <.input field={@form[:first_name]} type="text" label="First name" required />
-        <.input field={@form[:last_name]} type="text" label="Last name" required />
-        <.input field={@form[:email]} type="email" label="Email" required />
-        <.input field={@form[:password]} type="password" label="Password" required />
-
-        <:actions>
-          <.button phx-disable-with="Creating account..." class="w-full">Create an account</.button>
-        </:actions>
-      </.simple_form>
-    </div>
-    """
-  end
-
+  @impl Phoenix.LiveView
   def mount(_params, _session, socket) do
     changeset = Accounts.change_user_registration(%User{})
 
@@ -52,10 +15,33 @@ defmodule EdvitalHubWeb.UserRegistrationLive do
       |> assign(trigger_submit: false, check_errors: false)
       |> assign_form(changeset)
       |> assign(page_title: "Register")
+      |> assign(:template, :new)
 
     {:ok, socket, temporary_assigns: [form: nil]}
   end
 
+  @impl Phoenix.LiveView
+  def render(%{template: :new} = assigns) do
+    ~H"""
+    <AuthComponents.auth_template>
+      <.live_component
+        module={UserRegistration.New}
+        id="registration_form"
+        form={@form}
+        check_errors={@check_errors}
+        trigger_submit={@trigger_submit}
+      />
+    </AuthComponents.auth_template>
+    """
+  end
+
+  def render(%{template: :success} = assigns) do
+    ~H"""
+    <AuthComponents.registration_success_page form={@form} title="Confirm your Email Address" />
+    """
+  end
+
+  @impl Phoenix.LiveView
   def handle_event("save", %{"user" => user_params}, socket) do
     case Accounts.register_user(user_params) do
       {:ok, user} ->
@@ -66,7 +52,12 @@ defmodule EdvitalHubWeb.UserRegistrationLive do
           )
 
         changeset = Accounts.change_user_registration(user)
-        {:noreply, socket |> assign(trigger_submit: true) |> assign_form(changeset)}
+
+        {:noreply,
+         socket
+         |> assign(trigger_submit: true)
+         |> assign_form(changeset)
+         |> assign(:template, :success)}
 
       {:error, %Ecto.Changeset{} = changeset} ->
         {:noreply, socket |> assign(check_errors: true) |> assign_form(changeset)}
