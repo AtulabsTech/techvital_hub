531be3790e556e65eea90d6163f08111f2f1d83f
Rowa <jrowah@gmail.com>
2025-05-21 12:53:26 +0300

    implement show and hide password functionality

--- lib/techvital_hub_web/live/onboarding_live/shared/input_components.ex
+++ lib/techvital_hub_web/live/onboarding_live/shared/input_components.ex
@@ -3,6 +3,7 @@ defmodule TechvitalHubWeb.OnboardingLive.Shared.InputComponents do
 
   use Phoenix.Component
 
+  alias Phoenix.LiveView.JS
   alias TechvitalHubWeb.CoreComponents
 
   @type assigns :: map()
@@ -88,6 +89,11 @@ defmodule TechvitalHubWeb.OnboardingLive.Shared.InputComponents do
   end
 
   def input_field(%{label_type: "icon"} = assigns) do
+    assigns =
+      assign_new(assigns, :reveal_id, fn ->
+        :crypto.strong_rand_bytes(5) |> Base.encode16(case: :lower) |> binary_part(0, 5)
+      end)
+
     ~H"""
     <div phx-feedback-for={@name} class={@class}>
       <.label
@@ -97,18 +103,39 @@ defmodule TechvitalHubWeb.OnboardingLive.Shared.InputComponents do
       >
         {@label}
       </.label>
-      <input
-        type={@type}
-        name={@name}
-        id={@id}
-        value={Phoenix.HTML.Form.normalize_value(@type, @value)}
-        class={[
-          "input-field mt-2 block w-full rounded-lg text-zinc-900 focus:ring-0 sm:leading-6 phx-no-feedback:border-zinc-300 phx-no-feedback:focus:border-zinc-400 border-zinc-300 focus:border-zinc-400 sm:text-sm sm:leading-6 py-4",
-          @errors == [] && "placeholder:text-gray-500",
-          @errors != [] && "placeholder:text-red-500 text-error"
-        ]}
-        {@rest}
-      />
+      <div class="relative mt-2 ">
+        <input
+          type={@type}
+          name={@name}
+          id={@id}
+          data-reveal-id={@reveal_id}
+          value={Phoenix.HTML.Form.normalize_value(@type, @value)}
+          class={[
+            "input-field mt-2 block w-full rounded-lg text-zinc-900 focus:ring-0 sm:leading-6 phx-no-feedback:border-zinc-300 phx-no-feedback:focus:border-zinc-400 border-zinc-300 focus:border-zinc-400 sm:text-sm sm:leading-6 py-4",
+            @errors == [] && "placeholder:text-gray-500",
+            @errors != [] && "placeholder:text-red-500 text-error"
+          ]}
+          {@rest}
+        />
+        <div class="absolute inset-y-0 right-0 pr-3 flex items-center">
+          <button
+            type="button"
+            phx-click={
+              JS.toggle_class("hero-eye-slash", to: "#eye-icon-#{@reveal_id}")
+              |> JS.toggle_class("hero-eye", to: "#eye-icon-#{@reveal_id}")
+              |> JS.toggle_attribute({"type", "password", "text"},
+                to: "input[data-reveal-id='#{@reveal_id}']"
+              )
+            }
+          >
+            <CoreComponents.icon
+              name="hero-eye-slash"
+              id={"eye-icon-#{@reveal_id}"}
+              class="h-5 w-f cursor-pointer"
+            />
+          </button>
+        </div>
+      </div>
       <%= if @custom_errors do %>
         {render_slot(@error_messages)}
       <% else %>
